bbf7259fa10eedb13a331fa2e488c018470eb829
tcaric <tonci.caric@gmail.com>
2018-09-10 15:37:55 +0200

    Category Route implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 // 20180909 19:38
 // Spec 1. The appBar text should say 'Unit Converter' with font size 30.0 and na elevation of 0.o
 import 'package:flutter/material.dart';
-import './category.dart';
+import './categoryRoute.dart';
+
+/* import './category.dart';
 
 const _categoryName = 'Alarm';
-const _categoryIcon = Icons.alarm;
-const _categoryColor = Colors.green;
+const _categoryIcon = Icons.airplay;
+const _categoryColor = Colors.green; */
 
 void main() => runApp(UnitConverterApp());
 
@@ -15,16 +17,16 @@ class UnitConverterApp extends StatelessWidget {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: new Scaffold(
+      home: CategoryRoute(),
+    /*   home: new Scaffold(
         backgroundColor: Colors.green[100],
         body: Center(
           child: Category(
             name: _categoryName,
             color: _categoryColor,
             iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+          ), */
     );
   }
 }
+
